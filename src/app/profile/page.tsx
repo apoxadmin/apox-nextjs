ed05316229fd0f7d59e7c1be5b3bea16fa3b88fa
anderson-truong <andersonleetruong@gmail.com>
2024-02-17 18:22:20 -0800

    responsive profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,7 +9,7 @@ export default function ProfilePage() {
 
     return (
         <main className="flex min-h-screen flex-col items-center space-y-8 py-8 px-4 md:p-24">
-            <h1 className="text-4xl">My Profile</h1>
+            <h1 className="text-2xl md:text-4xl">My Profile</h1>
             <div className="w-full max-w-sm">
                 { <ProfileForm /> }
             </div>
