df334e771ad07ee9993cbfa0f6b887e5918957a7
anderson-truong <andersonleetruong@gmail.com>
2024-02-25 17:58:04 -0800

    home scrol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,66 +8,69 @@ import { Cinzel } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const cinzel = Cinzel({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className="flex max-h-screen flex-col items-center space-y-8 py-10 md:p-24">
-            <div className="flex flex-col items-center">
-                <h1 className={
-                    cn(
-                        cinzel.className,
-                        'text-2xl md:text-4xl text-indigo-700'
-                    )
-                }>
-                    Alpha Phi Omega
-                </h1>
-                <h1 className={
-                    cn(
-                        cinzel.className,
-                        'text-xl sm: text-2xl text-indigo-700'
-                    )
-                }>
-                    Chi Chapter
-                </h1>
-            </div>
-            <Carousel
-                opts={{
-                    align: 'start',
-                    loop: true
-                }}
-                plugins={[
-                    Autoplay({
-                        delay: 5000
-                    })
-                ]}
-            >
-                <CarouselContent>
-                    <CarouselItem className="flex justify-center">
-                        <img src="bleach.jpg" alt="bleach" className="h-[200px] md:h-[500px]"/>
-                    </CarouselItem>
-                    <CarouselItem className="flex justify-center">
-                    <img src="bestvps.jpg" alt="bleach" className="h-[200px] md:h-[500px]"/>
-                    </CarouselItem>
-                </CarouselContent>
-            </Carousel>
-            <div className="flex space-x-1 text-sm md:text-base">
-                <h1 className="italic text-neutral-400">
-                    sometimes you
-                </h1>
-                <h1 className="font-bold italic text-neutral-400">
-                    can
-                </h1>
-                <h1 className="italic text-neutral-400">
-                    capture greatness
-                </h1>
-            </div>
-            <Link href="/login">
-                <Button className="bg-indigo-600 hover:bg-indigo-500">
-                    Login
-                </Button>
-            </Link>
-        </main>
+        <ScrollArea>
+            <main className="flex max-h-screen flex-col items-center space-y-8 py-10 md:p-24">
+                <div className="flex flex-col items-center">
+                    <h1 className={
+                        cn(
+                            cinzel.className,
+                            'text-2xl md:text-4xl text-indigo-700'
+                        )
+                    }>
+                        Alpha Phi Omega
+                    </h1>
+                    <h1 className={
+                        cn(
+                            cinzel.className,
+                            'text-xl sm: text-2xl text-indigo-700'
+                        )
+                    }>
+                        Chi Chapter
+                    </h1>
+                </div>
+                <Carousel
+                    opts={{
+                        align: 'start',
+                        loop: true
+                    }}
+                    plugins={[
+                        Autoplay({
+                            delay: 5000
+                        })
+                    ]}
+                >
+                    <CarouselContent>
+                        <CarouselItem className="flex justify-center">
+                            <img src="bleach.jpg" alt="bleach" className="h-[200px] md:h-[500px]"/>
+                        </CarouselItem>
+                        <CarouselItem className="flex justify-center">
+                        <img src="bestvps.jpg" alt="bleach" className="h-[200px] md:h-[500px]"/>
+                        </CarouselItem>
+                    </CarouselContent>
+                </Carousel>
+                <div className="flex space-x-1 text-sm md:text-base">
+                    <h1 className="italic text-neutral-400">
+                        sometimes you
+                    </h1>
+                    <h1 className="font-bold italic text-neutral-400">
+                        can
+                    </h1>
+                    <h1 className="italic text-neutral-400">
+                        capture greatness
+                    </h1>
+                </div>
+                <Link href="/login">
+                    <Button className="bg-indigo-600 hover:bg-indigo-500">
+                        Login
+                    </Button>
+                </Link>
+            </main>
+        </ScrollArea>
     );
 }
