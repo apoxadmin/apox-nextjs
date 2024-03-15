ee8fecaebad48c06e2110c6e5dc26c1f90010a81
anderson-truong <andersonleetruong@gmail.com>
2024-03-15 04:08:10 -0700

    fix bad bug

--- src/app/calendar/EventCalendar/EventCard.tsx
+++ src/app/calendar/EventCalendar/EventCard.tsx
@@ -88,7 +88,7 @@ function EventCardDetail({ event, chairs, attendees, userData, setAttendees, set
                         Leave
                     </Button>
                     :
-                    userData && ((isBefore(endOfToday(), subDays(event?.startDate, 3)) && attendees.length < event.limit) || (event?.limit == 0) || DEBUG) && 
+                    userData && ((isBefore(endOfToday(), subDays(event?.startDate, 3)) && (attendees.length < event.limit || event?.limit == 0) ) || DEBUG) && 
                     <Button 
                         onClick={() => { 
                             signUpEvent(event?.id)
