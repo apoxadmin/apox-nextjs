000d0fbf03abcfa9c85d95a22838c9318a709c84
anderson-truong <andersonleetruong@gmail.com>
2024-04-02 14:24:55 -0700

    test game

---
+++ src/app/game/page.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import * as React from 'react';
+
+export default function GamePage() {
+    const [startTime, setStartTime] = React.useState<Date>(null);
+    const [endTime, setEndTime] = React.useState<Date>(null);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center space-y-8 py-10 md:py-24">
+            <h1>
+                hi
+            </h1>
+            <h1>
+                {JSON.stringify(startTime)}
+            </h1>
+            <h1>
+                {JSON.stringify(endTime)}
+            </h1>
+
+            <button
+                className="bg-lime-300 hover:bg-lime-400 p-10 rounded-full shadow-md"
+                onTouchStart={(e) => {
+                    e.preventDefault();
+                    setStartTime(new Date());
+                }}
+                onTouchEnd={(e) => {
+                    e.preventDefault();
+                    setEndTime(new Date());
+                    console.log('YO');
+                }}
+            >
+                hold here ;)
+            </button>
+        </main>
+    )
+}
