46017190179c731f7b26e3672e4f41377363e819
anderson-truong <andersonleetruong@gmail.com>
2024-04-02 14:43:18 -0700

    game: add timers

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -6,7 +6,19 @@ import * as React from 'react';
 export default function GamePage() {
     const [startTime, setStartTime] = React.useState<Date>(null);
     const [endTime, setEndTime] = React.useState<Date>(null);
+    const [longestTime, setLongestTime] = React.useState<number>(0);
     const [timeHeld, setTimeHeld] = React.useState<number>(0);
+    const [pressing, setPressing] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            if (pressing) {
+                setTimeHeld(differenceInMilliseconds(new Date(), startTime));
+            }
+        }, 10);
+
+        return () => clearInterval(interval);
+    })
 
     return (
         <main className="flex min-h-screen flex-col items-center space-y-8 py-10 md:py-24">
@@ -14,7 +26,10 @@ export default function GamePage() {
                 hi
             </h1>
             <h1>
-                Longest Held: {timeHeld/1000}s
+                Longest Time: {longestTime/1000}s
+            </h1>
+            <h1>
+                Current Time: {timeHeld/1000}s
             </h1>
 
             <button
@@ -22,22 +37,30 @@ export default function GamePage() {
                 onMouseDown={(e) => {
                     e.preventDefault();
                     setStartTime(new Date());
+                    setPressing(true);
                 }}
                 onMouseUp={(e) => {
+                    setPressing(false);
                     e.preventDefault();
                     const end = new Date()
                     setEndTime(end);
-                    setTimeHeld(differenceInMilliseconds(end, startTime));
+                    if (timeHeld > longestTime) {
+                        setLongestTime(timeHeld);
+                    }
                 }}
                 onTouchStart={(e) => {
+                    setPressing(true);
                     e.preventDefault();
                     setStartTime(new Date());
                 }}
                 onTouchEnd={(e) => {
+                    setPressing(false);
                     e.preventDefault();
                     const end = new Date()
                     setEndTime(end);
-                    setTimeHeld(differenceInMilliseconds(end, startTime));
+                    if (timeHeld > longestTime) {
+                        setLongestTime(timeHeld);
+                    }
                 }}
             >
                 hold here ;)
