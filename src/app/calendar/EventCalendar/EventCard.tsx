fb1c4533764a32e9581b9a17c0b79b3c542e4c41
anderson-truong <andersonleetruong@gmail.com>
2024-05-06 16:27:19 -0700

    change signup to none

--- src/app/calendar/EventCalendar/EventCard.tsx
+++ src/app/calendar/EventCalendar/EventCard.tsx
@@ -90,7 +90,7 @@ function EventCardDetail({ event, chairs, attendees, userData, setAttendees, set
                         Leave
                     </Button>
                     :
-                    userData && ((isBefore(endOfToday(), subDays(event?.startDate, 3)) && (attendees.length < event.limit || event?.limit == 0) ) || DEBUG) && 
+                    userData && ((isBefore(endOfToday(), subDays(event?.startDate, 0)) && (attendees.length < event.limit || event?.limit == 0) ) || DEBUG) && 
                     <Button 
                         onClick={() => { 
                             signUpEvent(event?.id)
