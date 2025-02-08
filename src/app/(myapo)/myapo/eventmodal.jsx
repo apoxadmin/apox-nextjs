5ffaa7a56a82a0c2d5568e41a76d8fe75c23ab2f
apaphy <82987703+apaphy@users.noreply.github.com>
2025-02-07 16:12:36 -0800

    fix shifts brocken dhmu

--- src/app/(myapo)/myapo/eventmodal.jsx
+++ src/app/(myapo)/myapo/eventmodal.jsx
@@ -227,7 +227,7 @@ function AttendeeInfo({ isShift, supabase, event, userData, shiftNum })
     }, [ event ]);
     
     return ( 
-    <div className={`swiper-no-swiping ${isShift ? "bg bg-[#252a30] p-2 rounded-xl" : ""}`}>
+    <div className={`swiper-no-swiping bg-white dark:bg-[#252a30] p-2 rounded-xl text-white dark:text-slate-300`}>
         <div className="flex flex-col space-y-2 overflow-auto max-h-[40vh]">
             {
                     isShift ?
