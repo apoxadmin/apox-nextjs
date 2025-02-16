c19e4687ea6ac7dee71e355333fc38b6d29a5777
Vikram <alexgiantseed@gmail.com>
2025-02-15 17:50:47 -0800

    yay first commit - constitution link
    added Constitution to sidebar links

--- src/app/(myapo)/sidebar.jsx
+++ src/app/(myapo)/sidebar.jsx
@@ -1,51 +1,67 @@
-'use client'
+"use client";
 
-import { isPrivileged } from "@/supabase/user"
-import Link from "next/link"
+import { isPrivileged } from "@/supabase/user";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
-const buttonClassName = "px-4 py-4 rounded-lg hover:text-neutral-800 hover:bg-neutral-200 hover:cursor-pointer transition ease-out delay-20 duration-150"
+const buttonClassName =
+	"px-4 py-4 rounded-lg hover:text-neutral-800 hover:bg-neutral-200 hover:cursor-pointer transition ease-out delay-20 duration-150";
 
 const openInNewTab = (url) => {
-    const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-    if (newWindow) newWindow.opener = null
-  }
+	const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+	if (newWindow) newWindow.opener = null;
+};
 
 function SidebarButton({ name, url = "/" }) {
-    return (
-        <Link href={url} className={buttonClassName}>
-            {name}
-        </Link>
-    )
+	return (
+		<Link href={url} className={buttonClassName}>
+			{name}
+		</Link>
+	);
 }
 function SidebarLink({ name, url = "/" }) {
-    return (
-        <Link href={url} className={buttonClassName} rel="noopener noreferrer" target='_blank'>
-            {name}
-        </Link>
-    )
+	return (
+		<Link
+			href={url}
+			className={buttonClassName}
+			rel="noopener noreferrer"
+			target="_blank"
+		>
+			{name}
+		</Link>
+	);
 }
 
 export default function Sidebar({ visible = true }) {
-    return (
-        <div className={`min-w-[240px] ${visible ? '' : 'hidden'}`}>
-            <div className="flex flex-col text-neutral-500">
-                {/* <SidebarButton name="Crossword" url="/crossword" /> */}
-                {/* <SidebarLink name="Gas Reimbursement" url="https://docs.google.com/forms/d/e/1FAIpQLScp47-NYgZm-Gp7XGkTf7jYeHHdinqfI2TZ9KkI4wQ1yXEexA/viewform?usp=sf_link" />
+	return (
+		<div className={`min-w-[240px] ${visible ? "" : "hidden"}`}>
+			<div className="flex flex-col text-neutral-500">
+				{/* <SidebarButton name="Crossword" url="/crossword" /> */}
+				{/* <SidebarLink name="Gas Reimbursement" url="https://docs.google.com/forms/d/e/1FAIpQLScp47-NYgZm-Gp7XGkTf7jYeHHdinqfI2TZ9KkI4wQ1yXEexA/viewform?usp=sf_link" />
                 <SidebarLink name="Regular Reimbursement" url="https://docs.google.com/forms/d/e/1FAIpQLSco5IJ_mG_dN8xNbPuDKF-P19KA1GXa88KaWgqoXysYMhsMvw/viewform?usp=sf_link" />
                 <SidebarLink name="Rule Violations" url="https://docs.google.com/forms/d/e/1FAIpQLSfcZlkaLwu42CiQLi_9exH5A100DGQu5VPJWkzvhw0ChYWucA/viewform" /> */}
-                <SidebarLink name="Test Bank Request Form" url="https://forms.gle/Scb8DBhEtusHcHeY8" />
-                <SidebarLink name="Collaborative Drive" url="https://drive.google.com/drive/folders/1qBdA5DpNJLhywhIyjHTC-lY3FZVVcmZ0?usp=sharing" />
-                {/* <SidebarLink name="Outside Service" url="https://forms.gle/UuSLmX42myFNF8sx9" />
+				<SidebarLink
+					name="Test Bank Request Form"
+					url="https://forms.gle/Scb8DBhEtusHcHeY8"
+				/>
+				<SidebarLink
+					name="Collaborative Drive"
+					url="https://drive.google.com/drive/folders/1qBdA5DpNJLhywhIyjHTC-lY3FZVVcmZ0?usp=sharing"
+				/>
+				<SidebarLink
+					name="Constitution"
+					url="https://docs.google.com/document/d/16X9QcZW2fyaNhXL24gkg9VghSQVVTqJo4xAyC17S-2w/edit?usp=sharing"
+				/>
+				{/* <SidebarLink name="Outside Service" url="https://forms.gle/UuSLmX42myFNF8sx9" />
                 <SidebarLink name="Big/Little Comp" url="https://docs.google.com/spreadsheets/d/12U-2avpz5loeUF7Wf4trf_xXwhSaTnVukvJsh0COvfs/edit?usp=sharing" /> */}
-                {/*<SidebarButton name="My Status" />
+				{/*<SidebarButton name="My Status" />
                 <SidebarButton name="My Family" />
                 <SidebarButton name="My Class" />
                 <SidebarButton name="Resources" />
                 <SidebarButton name="APOChat" />
                 <SidebarButton name="Settings" />
                 */}
-            </div>
-        </div>
-    )
+			</div>
+		</div>
+	);
 }
--- src/supabase/auth.js
+++ src/supabase/auth.js
@@ -1,32 +1,34 @@
-'use server'
+"use server";
 
 import { revalidatePath } from "next/cache";
-import { createSupabaseServer } from "./server"
+import { createSupabaseServer } from "./server";
 import { redirect } from "next/navigation";
 
 export async function loginUserWithEmailAndPassword(email, password) {
-    const supabase = createSupabaseServer();
+	const supabase = createSupabaseServer();
+	const { error } = await supabase.auth.signInWithPassword({
+		email,
+		password,
+	});
 
-    const { error } = await supabase.auth.signInWithPassword({ email, password });
-
-    if (!error) {
-        //revalidatePath('/myapo', 'layout');
-        redirect('/myapo');
-    } else {
-        console.log(error);
-        return Promise.reject(new Error('User does not exist.'));
-    }
+	if (!error) {
+		//revalidatePath('/myapo', 'layout');
+		redirect("/myapo");
+	} else {
+		console.log(error);
+		return Promise.reject(new Error("User does not exist."));
+	}
 }
 
 export async function logout() {
-    const supabase = createSupabaseServer();
+	const supabase = createSupabaseServer();
 
-    const { error } = await supabase.auth.signOut();
+	const { error } = await supabase.auth.signOut();
 
-    if (!error) {
-        revalidatePath('/', 'layout');
-        redirect('/');
-    } else {
-        return Promise.reject(new Error('Cannot log out.'));
-    }
+	if (!error) {
+		revalidatePath("/", "layout");
+		redirect("/");
+	} else {
+		return Promise.reject(new Error("Cannot log out."));
+	}
 }
