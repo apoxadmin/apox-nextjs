ea7200818296623a50bcf90d86f5acd68cc571f7
APO-X Admin VP <apoxadmin@gmail.com>
2026-04-22 16:39:42 -0700

    BL_pledge

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <>
     <Navbar />
     <div className="grow flex flex-col items-center space-y-4">
-        <div className="grow relative p-10 w-full flex flex-col items-center justify-evenly bg-no-repeat bg-[linear-gradient(to_bottom,rgba(20,20,20,0.3),rgba(1,1,1,0.65),rgba(1,1,1,0.9)),url('/bl_pledge.JPG')] bg-cover bg-[50%_30%]">
+        <div className="grow relative p-10 w-full flex flex-col items-center justify-evenly bg-no-repeat bg-[linear-gradient(to_bottom,rgba(20,20,20,0.3),rgba(1,1,1,0.65),rgba(1,1,1,0.9)),url('/BL_pledge.JPG')] bg-cover bg-[50%_30%]">
           <div className={`mt-16z-10 flex items-center text-white text-4xl space-x-8 [&>*]:drop-shadow-lg ${titleFont.className}`}>
             <p>Be a leader.</p>
             <p>Be a friend.</p>
